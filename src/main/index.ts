0b527ae6c08644f1f311d051e529dfb74bc06998
Cherylynn Amaryliss Munai Anak Cassidy <104402202@students.swinburne.edu.my>
2026-03-15 07:43:51 +0800

    Update

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,10 +2,10 @@ import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -27,8 +27,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -36,60 +34,76 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
   ipcMain.on('ping', () => console.log('pong'))
 
   createWindow()
 
-  autoUpdater.checkForUpdatesAndNotify()
+  // -----------------------------
+  // AUTO UPDATE CONFIGURATION
+  // -----------------------------
+
+  autoUpdater.logger = log
+  autoUpdater.logger.transports.file.level = 'info'
+
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...')
+  })
 
   autoUpdater.on('update-available', () => {
+    console.log('Update available')
     dialog.showMessageBox({
       type: 'info',
       title: 'Update Available',
-      message: 'A new update is available. Downloading now.'
+      message: 'A new version of Todo Desktop is available. Downloading now.'
     })
   })
 
+  autoUpdater.on('update-not-available', () => {
+    console.log('No updates available')
+  })
+
+  autoUpdater.on('error', (err) => {
+    console.log('Update error:', err)
+  })
+
   autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded')
+
     dialog.showMessageBox({
       type: 'info',
       title: 'Update Ready',
-      message: 'Update downloaded. Restarting to install.'
+      message: 'Update downloaded. The application will restart to install the update.'
     }).then(() => {
       autoUpdater.quitAndInstall()
     })
   })
 
+  // wait 5 seconds before checking
+  console.log('[AutoUpdater] App version:', app.getVersion())
+  console.log('[AutoUpdater] Is dev?', is.dev)
+  console.log('[AutoUpdater] Will check in 5 seconds...')
+  
+  setTimeout(() => {
+    console.log('[AutoUpdater] Checking for updates now...')
+    autoUpdater.checkForUpdates()
+  }, 5000)
+
+  // -----------------------------
+
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
